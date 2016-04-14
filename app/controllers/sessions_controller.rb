d40481fdb9e5a4e9a066ab380d3240b5d97882f3
mokutsu <misato.okutsu@gmail.com>
2016-04-14 17:08:37 -0400

    log in and log out added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
+    customer = Customer.find_by(email: params[:email])
+    if customer && customer.authenticate(params[:password])
+      session[:customer_id] = customer.id
+      redirect_to restaurants_path, notice: "Logged in!"
+    else flash[:alert] = "user and/or password is incorrect"
+      render :new
+    end
   end
 
   def destroy
+    session[:customer_id] = nil
+    redirect_to restaurants_path, notice: "Logged out!"
   end
 end
