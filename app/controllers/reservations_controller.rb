7fc6c6a3a269e5bf8a844dd3964cd71ddf7114d3
mokutsu <misato.okutsu@gmail.com>
2016-04-14 17:51:18 -0400

    added reservation form. showing reservation and controller for reservation to be updated

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,7 @@
 class ReservationsController < ApplicationController
+
   def create
+    redirect_to restaurants_path
   end
+
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,13 @@
 class RestaurantsController < ApplicationController
   def show
+    @restaurant = Restaurant.find(params[:id])
+    @reservation = Reservation.new(restaurant_id: params[:id] )
 
   end
 
   def index
     @restaurants = Restaurant.all
   end
+
+
 end
