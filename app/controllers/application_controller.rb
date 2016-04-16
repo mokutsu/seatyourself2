bf0258453f360ea53a51ba4f76c547049070b98c
mokutsu <misato.okutsu@gmail.com>
2016-04-16 14:04:58 -0400

    if session[:user_id] added to current_user method in applications controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+private
   def current_customer
-    @current_customer ||= session[:customer_id] && Customer.find(session[:customer_id])
+    @current_customer ||= session[:customer_id] && Customer.find(session[:customer_id]) if session[:customer_id]
   end
-
+    
   helper_method :current_customer
 
   def ensure_logged_in
