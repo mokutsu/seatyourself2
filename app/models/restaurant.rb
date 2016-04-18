6c240e177f84248a84905e1aa52eedf4f533e96c
mokutsu <misato.okutsu@gmail.com>
2016-04-18 16:13:20 -0400

    added party size validation in restaurant model, reservation model, modified seed file

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,26 @@ class Restaurant < ActiveRecord::Base
   belongs_to :category
 
   validates :name, presence: true
+
+
+  def availability(party_size, time)
+     current_capacity(time) - party_size
+   end
+
+
+
+   private
+   def current_capacity(time)
+     if self.reservations.empty? 
+       capacity
+     else
+     capacity - reserved_capacity(time)
+    end
+   end
+
+   def reserved_capacity(time)
+     self.reservations.where(time: time.beginning_of_hour..time.end_of_hour).sum(:party_size)
+   end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,5 @@ burger_king = Restaurant.create(name: 'Burger King', opening: '2000-01-01 12:00:
 taco_bell = Restaurant.create(name: 'Taco Bell', opening: '2000-01-01 12:00:00' , closing: '2000-01-01 23:59:00', img_link: "http://static1.squarespace.com/static/5444024ee4b02c9e7d3c7f22/544eac9ae4b00059eab2f238/544eac9ce4b0bca1f95a84c5/1414442142018/slider_tacos3.png", capacity: 100, category_id: 4)
 subway = Restaurant.create(name: 'Subway', opening: '2000-01-01 12:00:00' , closing: '2000-01-01 23:59:00', img_link: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Subway-Restaurant_in_Passau.jpg", capacity: 20, category_id: 1)
 kupert_kim = Restaurant.create(name:'Kupert and Kim', opening: '2000-01-01 12:00:00' , closing: '2000-01-01 23:59:00', img_link: "https://pbs.twimg.com/profile_images/3099595511/f3196239dc69a368b72d8928590decc1_400x400.png", capacity: 1000, category_id: 2)
+
+reservation1 = Reservation.create(customer_id: 1, restaurant_id: 1, date: '2016-05-01', time: '2000-01-01 16:00:00', party_size: 5)
