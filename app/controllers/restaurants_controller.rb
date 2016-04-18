a7699c6946089005a9c8e38c1b9e678e45638ef7
mokutsu <misato.okutsu@gmail.com>
2016-04-18 17:19:18 -0400

    further modifications to time selector in reservations form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,7 +3,12 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.find(params[:id])
     @reservation = Reservation.new(restaurant_id: params[:id] )
 
-
+    @form_time_range = []
+      time_option = @restaurant.opening
+    while time_option < @restaurant.closing  
+      @form_time_range << time_option
+      time_option = time_option+1.hour
+    end
   end
 
   def index
