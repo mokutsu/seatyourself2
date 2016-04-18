3d3eca34b00d55366d87999a20e54eefad226e02
mokutsu <misato.okutsu@gmail.com>
2016-04-18 14:42:13 -0400

    password validation lines added

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -7,12 +7,5 @@ class Reservation < ActiveRecord::Base
 
   validates :restaurant_id, :customer_id, :date, :time, presence: true
 
-  validates :date, numericality: {greater_than: Proc.new{ |date| Date.today}} <__ DOESN"T WORK!!!
-
-
-#   current_date = Time.now.strfrtime("%Y-%m-%d")
-#
-# >> Time.now.strftime("%d/%m/%Y %H:%M")
-# => "14/09/2011 14:09"
 
 end
