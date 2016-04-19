928323b1bcfe34152ac3e0fbba812007a86af3f9
Guin <guinifer.k@gmail.com>
2016-04-19 12:41:31 -0400

    refactored reservations -GUIN

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,13 +1,14 @@
 class ReservationsController < ApplicationController
-  before_action :load_restaurant
+  before_action :load_reservation, only: [:show, :edit, :update, :destroy ]
+  before_action :load_restaurant, except: [:create, ]
   before_action :ensure_logged_in
 
   def create
+
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @reservation = @restaurant.reservations.build(reservation_params)
+    
     @reservation.customer = current_customer
-
-
-
     if @reservation.save
       redirect_to restaurants_url, notice: "Reservation successful! Mission accomplished."
     else
@@ -17,39 +18,34 @@ class ReservationsController < ApplicationController
   end
 
   def show
-    @reservation = Reservation.find(params[:id])
-    @restaurant = Restaurant.find_by(id: @reservation.restaurant_id)
-end
+  end
 
   def edit
-    @reservation = Reservation.find(params[:id])
-    @restaurant = Restaurant.find_by(id: @reservation.restaurant_id)
   end
 
   def update
-    @reservation = Reservation.find(params[:id])
     if @reservation.update_attributes(reservation_params)
       redirect_to customer_path(current_customer), notice: "Reservation successfully updated!"
     else
-      @reservation = Reservation.find(params[:id])
+      load_reservation
       render 'reservations/show', alert: "Error: Please try again."#restaurant_path(@restaurant), notice: "Error: Please try again."
     end
   end
 
   def destroy
-    @reservation = Reservation.find(params[:id])
-    @restaurant = Restaurant.find_by(id: @reservation.restaurant_id)
-
     @reservation.destroy
     redirect_to customer_path(current_customer), notice: "Reservation successfully deleted."
   end
+
   private
   def reservation_params
     params.require(:reservation).permit(:date, :time, :restaurant_id, :party_size)
   end
-
   def load_restaurant
-    @restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant = Restaurant.find_by(id: @reservation.restaurant_id)
+  end
+  def load_reservation
+    @reservation = Reservation.find(params[:id])
   end
 
 end
