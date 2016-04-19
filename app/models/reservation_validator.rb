94b3380e6e1b1c67ccaf0696c9d6726ad093cb4e
Guin <guinifer.k@gmail.com>
2016-04-19 09:01:10 -0400

    f

--- app/models/reservation_validator.rb
+++ app/models/reservation_validator.rb
@@ -1,13 +1,10 @@
 class ReservationValidator < ActiveModel::Validator
   def validate(reservation)
-    #binding.pry
-#this works, there is an issue with timezones in the seed
     to_early = reservation.time < reservation.restaurant.opening
     to_late = reservation.time > reservation.restaurant.closing
    reservation.errors[:open] << "The Restaurant is not open at this time" if to_early || to_late
 
 
-  #  binding.pry
 
   end
 
