dcd7d164f3559c7817f739fff46274b8473cfe0a
mokutsu <misato.okutsu@gmail.com>
2016-04-25 20:21:43 -0400

    customer model validations added, minor formatting - mo

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -5,32 +5,27 @@ class Reservation < ActiveRecord::Base
   belongs_to :customer
   belongs_to :restaurant
 
-def reservation_validation_list
-  check_presence
-  capacity_available
-  massive_partysize
-end
-
-  def check_presence
-      if :restaurant_id == nil || :customer_id == nil || :date == nil
-        errors.add(:field, "required")
-        return
-      end
+  def reservation_validation_list
+    # check_presence
+    capacity_available
+    massive_partysize
+  end
+    #
+    # def check_presence
+    #   if :restaurant_id == nil || :customer_id == nil || :date == nil
+    #     errors.add(:field, "this is needed!")
+    #     return
+    #   end
+    #   end
+    def capacity_available
+        if self.restaurant.availability(self.party_size, self.time) < 0
+          errors.add(:party_size, "Inadequate capacity")
+        end
     end
 
-
-  def capacity_available
-      if self.restaurant.availability(self.party_size, self.time) < 0
-        errors.add(:party_size, "Inadequate capacity")
-      end
-
-  end
-
-  def massive_partysize
-      if self.party_size > 30
-        errors.add(:party_size, "Party size exceeds 30, please call restaurant")
-      end
-  end
-
-
+    def massive_partysize
+        if self.party_size > 30
+          errors.add(:party_size, "Party size exceeds 30, please call restaurant")
+        end
+    end
 end
